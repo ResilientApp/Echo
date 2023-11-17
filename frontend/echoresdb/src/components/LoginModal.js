5b3751f7b6cc8556130014cae04b283b054eb8ce
ashw24 <ashchembu@gmail.com>
2023-11-17 14:41:15 -0800

    contact us

--- frontend/echoresdb/src/components/LoginModal.js
+++ frontend/echoresdb/src/components/LoginModal.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import './LoginModal.css'; // Assuming you have an external CSS file for styles
 
 const LoginModal = ({ close }) => {
   return (
     <div className="login-modal">
       <div className="login-modal-content">
-        <button className="login-modal-close" onClick={close}>&times;</button>
+      <button className="login-modal-close" onClick={close}>
+          <FontAwesomeIcon icon={faTimes} />
+        </button>        
         <button className="login-google-btn">
           <span>Sign in with Google</span>
           <FontAwesomeIcon icon={faGoogle} className="google-icon" />
