3de47880228b06872d6667a253c40d606af89783
ashw24 <ashchembu@gmail.com>
2023-12-02 16:49:09 -0800

    removed vestigial code

--- backend/controllers/dataController.js
+++ backend/controllers/dataController.js
@@ -78,81 +78,7 @@ function sendRequest(api, encodedImage) {
 
 
 
-const postdata = async (req, res) => {
-  const appId = process.env.APPID;
-  const hashToken = process.env.HASHTOKEN;
-  const tokenUrl = `https://api.iq.inrix.com/auth/v1/appToken?appId=${appId}&hashToken=${hashToken}`;
 
-  const { start, end } = req.body; // Extracting start and end from request body
-
-  try {
-    const tokenResponse = await fetch(tokenUrl);
-    if (!tokenResponse.ok) {
-      throw new Error('Failed to fetch token');
-    }
-
-    const tokenData = await tokenResponse.json();
-    const appToken = tokenData.result.token;
-
-    const routeUrl = `https://api.iq.inrix.com/findRoute?wp_1=${start.lat},${start.lng}&wp_2=${end.lat},${end.lng}&routeOutputFields=B%2CM%2CP%2CS%2CW&format=json`;
-
-
-    const routeResponse = await fetch(routeUrl, {
-      headers: {
-        'accept': 'application/json',
-        'Authorization': `Bearer ${appToken}`
-      }
-    });
-
-    if (!routeResponse.ok) {
-      throw new Error('Failed to fetch route information');
-    }
-
-    const routeData = await routeResponse.json();
-    res.json(routeData);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-
-
-const postdataDriver = async (req, res) => {
-  const appId = process.env.APPID;
-  const hashToken = process.env.HASHTOKEN;
-  const tokenUrl = `https://api.iq.inrix.com/auth/v1/appToken?appId=${appId}&hashToken=${hashToken}`;
-
-  const { userStart, userEnd, passengerStart, passengerEnd } = req.body;
-
-  try {
-    const tokenResponse = await fetch(tokenUrl);
-    if (!tokenResponse.ok) {
-      throw new Error('Failed to fetch token');
-    }
-
-    const tokenData = await tokenResponse.json();
-    const appToken = tokenData.result.token;
-
-    const routeUrl = `https://api.iq.inrix.com/findRoute?wp_1=${userStart.lat},${userStart.lng}&wp_2=${passengerStart.lat},${passengerStart.lng}&wp_3=${passengerEnd.lat},${passengerEnd.lng}&wp_4=${userEnd.lat},${userEnd.lng}&routeOutputFields=B%2CM%2CP%2CS%2CW&format=json`;
-
-
-    const routeResponse = await fetch(routeUrl, {
-      headers: {
-        'accept': 'application/json',
-        'Authorization': `Bearer ${appToken}`
-      }
-    });
-
-    if (!routeResponse.ok) {
-      throw new Error('Failed to fetch route information');
-    }
-
-    const routeData = await routeResponse.json();
-    res.json(routeData);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
 
 
 
@@ -203,7 +129,5 @@ const face = async (req, res) => {
 }
 
 module.exports = {
-  postdata,
-  postdataDriver,
   face
 };
--- backend/routes/data.js
+++ backend/routes/data.js
@@ -4,17 +4,14 @@ const router = express.Router()
 
 
 const { 
-  postdata,
-  postdataDriver,
+  
   face
 } = require("../controllers/dataController")
 
 
 //login route
 
-router.post("/", postdata) // to do a regular map
 
-router.post("/driver", postdataDriver)
 
 router.post("/face", face)
 
